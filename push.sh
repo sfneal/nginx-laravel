7712dc20a98da709a641a438943b2a9054d86a86
mrstephenneal <stephen.neal14@gmail.com>
2019-07-09 15:29:10 -0400

    FIX formatting of push script

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker push -t stephenneal/nginx-laravel:1.15-alpine
-docker push -t stephenneal/nginx-laravel:1.16-alpine
-docker push -t stephenneal/nginx-laravel:1.17
-docker push -t stephenneal/nginx-laravel:1.17-alpine-v1
-docker push -t stephenneal/nginx-laravel:1.17-alpine-v2
-docker push -t stephenneal/nginx-laravel:1.17-alpine-v3
+docker push stephenneal/nginx-laravel:1.15-alpine
+docker push stephenneal/nginx-laravel:1.16-alpine
+docker push stephenneal/nginx-laravel:1.17
+docker push stephenneal/nginx-laravel:1.17-alpine-v1
+docker push stephenneal/nginx-laravel:1.17-alpine-v2
+docker push stephenneal/nginx-laravel:1.17-alpine-v3
