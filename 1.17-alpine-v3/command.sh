751634f75e313fc3c1210bf8015adf8dd11f8f9f
mrstephenneal <stephen.neal14@gmail.com>
2019-07-09 14:52:36 -0400

    FIX aws s3 transactions

--- 1.17-alpine-v3/command.sh
+++ 1.17-alpine-v3/command.sh
@@ -2,20 +2,24 @@
 
 replace_domain --domain ${domain} --conf-file /etc/nginx/conf.d/default.conf
 
-# Make directory for dummy certificates
-# Create dummy certificate for ${domain}
-echo "Creating dummy certificate for ${domain}"
-mkdir -m 777 -p /etc/letsencrypt/live/${domain}/ \
-    && openssl req -x509 -nodes -newkey rsa:1024 -days 1 \
-        -keyout "/etc/letsencrypt/live/${domain}/privkey.pem" \
-        -out "/etc/letsencrypt/live/${domain}/fullchain.pem" \
-        -subj "/CN=localhost"
-
-# Check if aws_s3 is enabled
 if [ ${aws_s3} -gt 0 ]
 then
+    # Configure AWS creds
     sh /scripts/aws-credentials.sh
-    awss3 --bucket hpa-ssl-certs sync --local_path awsutils/s3
+
+    # Download certs from AWS S3
+    awss3 download --bucket ${aws_s3_bucket} \
+        --remote_path  \
+        --local_path /etc/letsencrypt/
+else
+    # Make directory for dummy certificates
+    # Create dummy certificate for ${domain}
+    echo "Creating dummy certificate for ${domain}"
+    mkdir -m 777 -p /etc/letsencrypt/live/${domain}/ \
+        && openssl req -x509 -nodes -newkey rsa:1024 -days 1 \
+            -keyout "/etc/letsencrypt/live/${domain}/privkey.pem" \
+            -out "/etc/letsencrypt/live/${domain}/fullchain.pem" \
+            -subj "/CN=localhost"
 fi
 
 # Start Nginx service
