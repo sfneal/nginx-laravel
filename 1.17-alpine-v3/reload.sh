a82536257fae7116198636f2949a37b9b6cef6c0
mrstephenneal <stephen.neal14@gmail.com>
2019-07-15 10:56:33 -0400

    FIX conditional to use aws_s3 env var to determine environment

--- 1.17-alpine-v3/reload.sh
+++ 1.17-alpine-v3/reload.sh
@@ -4,12 +4,12 @@
 nginx -s reload
 
 # Upload certs to AWS S3
-# Check if staging is enabled via ENV vars (if staging == 0)
-if [[ ${staging} -ne 0 ]];
+# Check if aws_s3 is enabled via ENV vars (if staging != 0)
+if [[ ${aws_s3} -ne 0 ]];
 then
+    echo "Staging environment enabled... skipping AWS S3 SSL certs upload."
+else
     echo "Production environment enabled... uploading SSL certs to AWS S3."
     awss3 upload --bucket ${aws_s3_bucket} \
         --local_path /etc/letsencrypt/
-else
-    echo "Staging environment enabled... skipping AWS S3 SSL certs upload."
 fi
