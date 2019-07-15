1a1ef7bc2fda79c68a55eb859708dd4bd24418d6
mrstephenneal <stephen.neal14@gmail.com>
2019-07-15 10:21:50 -0400

    REFORMAT comments

--- 1.17-alpine-v3/reload.sh
+++ 1.17-alpine-v3/reload.sh
@@ -7,8 +7,9 @@ nginx -s reload
 # Check if staging is enabled via ENV vars (if staging != 1)
 if [[ ${staging} -ne 1 ]];
 then
+    echo "Production environment enabled... uploading SSL certs to AWS S3."
     awss3 upload --bucket ${aws_s3_bucket} \
         --local_path /etc/letsencrypt/
 else
-    echo "Staging environment enabled... skipping AWS S3 SSL certs upload"
+    echo "Staging environment enabled... skipping AWS S3 SSL certs upload."
 fi
