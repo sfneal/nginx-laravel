828daade291d316f2ae3bcc5112ce0db38fc01b5
mrstephenneal <stephen.neal14@gmail.com>
2019-07-15 10:19:48 -0400

    FIX staging if statement to check if staging is disabled

--- 1.17-alpine-v3/reload.sh
+++ 1.17-alpine-v3/reload.sh
@@ -4,9 +4,11 @@
 nginx -s reload
 
 # Upload certs to AWS S3
-# Check if staging is enabled via ENV vars (if staging > 0)
-if [ ${staging} -gt 0 ]
+# Check if staging is enabled via ENV vars (if staging != 1)
+if [[ ${staging} -ne 1 ]];
 then
     awss3 upload --bucket ${aws_s3_bucket} \
         --local_path /etc/letsencrypt/
+else
+    echo "Staging environment enabled... skipping AWS S3 SSL certs upload"
 fi
