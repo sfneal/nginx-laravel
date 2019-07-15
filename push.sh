9cd8b5303dbf4de510655e3b3818c08494d28964
mrstephenneal <stephen.neal14@gmail.com>
2019-07-15 14:26:42 -0400

    FIX push script

--- push.sh
+++ push.sh
@@ -5,4 +5,5 @@ docker push stephenneal/nginx-laravel:1.16-alpine
 docker push stephenneal/nginx-laravel:1.17
 docker push stephenneal/nginx-laravel:1.17-alpine-v1
 docker push stephenneal/nginx-laravel:1.17-alpine-v2
+docker push stephenneal/nginx-laravel:1.17-alpine-v3
 docker push stephenneal/nginx-laravel:1.17-alpine-v4
