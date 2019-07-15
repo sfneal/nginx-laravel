3cbad7f5a506779cc234d1da13a86d43f4a86d73
mrstephenneal <stephen.neal14@gmail.com>
2019-07-15 13:42:38 -0400

    MAKE nginx-laravel:1.17-alpine-v4 with flask api enabled

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ docker build -t stephenneal/nginx-laravel:1.17 ./1.17/
 docker build -t stephenneal/nginx-laravel:1.17-alpine-v1 ./1.17-alpine-v1/
 docker build -t stephenneal/nginx-laravel:1.17-alpine-v2 ./1.17-alpine-v2/
 docker build -t stephenneal/nginx-laravel:1.17-alpine-v3 ./1.17-alpine-v3/
+docker build -t stephenneal/nginx-laravel:1.17-alpine-v4 ./1.17-alpine-v4/
