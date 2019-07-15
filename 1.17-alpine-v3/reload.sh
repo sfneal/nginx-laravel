4e0c97a16f73b4803b11319f2c3214d51ca4c79f
mrstephenneal <stephen.neal14@gmail.com>
2019-07-15 10:25:25 -0400

    FIX staging conditional to properly confirm staging enviroment enabled

--- 1.17-alpine-v3/reload.sh
+++ 1.17-alpine-v3/reload.sh
@@ -4,8 +4,8 @@
 nginx -s reload
 
 # Upload certs to AWS S3
-# Check if staging is enabled via ENV vars (if staging != 1)
-if [[ ${staging} -ne 1 ]];
+# Check if staging is enabled via ENV vars (if staging == 0)
+if [[ ${staging} -ne 0 ]];
 then
     echo "Production environment enabled... uploading SSL certs to AWS S3."
     awss3 upload --bucket ${aws_s3_bucket} \
