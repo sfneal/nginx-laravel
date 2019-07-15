7584f1d2260705304c866a56342e25ab179205c1
mrstephenneal <stephen.neal14@gmail.com>
2019-07-15 12:42:21 -0400

    ADD renewal conf downloading to start.sh

--- 1.17-alpine-v3/start.sh
+++ 1.17-alpine-v3/start.sh
@@ -5,6 +5,7 @@ replace_domain --domain ${domain} --conf-file /etc/nginx/conf.d/default.conf
 
 # Make directory for live SSL certs
 mkdir -m 777 -p /etc/letsencrypt/live/${domain}/
+mkdir -m 777 -p /etc/letsencrypt/renewal/${domain}/
 
 # Create dummy certificate for ${domain}
 echo "Creating dummy certificate for ${domain}"
@@ -25,6 +26,9 @@ then
         --local_path /etc/letsencrypt/live/${domain}/ \
         --remote_path live/${domain}/ \
         --recursive
+    awss3 download --bucket ${aws_s3_bucket} \
+        --local_path /etc/letsencrypt/renewal/${domain}/ \
+        --remote_path renewal/${domain}.conf
 fi
 
 # Start Nginx service
