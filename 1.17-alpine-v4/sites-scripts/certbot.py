fe7b7ae0f6f73b3626290f0a1842203aeb258286
mrstephenneal <stephen.neal14@gmail.com>
2019-07-16 09:50:48 -0400

    CUT unused arguments

--- 1.17-alpine-v4/sites-scripts/certbot.py
+++ 1.17-alpine-v4/sites-scripts/certbot.py
@@ -9,17 +9,11 @@ def main():
     usage = 'AWS S3 command-line-interface wrapper.'
     description = 'Execute AWS S3 commands.'
     helpers = {
-        'aws_s3': "Switch to enable/disabled AWS S3 actions.",
-        'aws_s3_bucket': "Name of the AWS S3 bucket SSL certs are stored in.",
         'domain': "List of domain names separated by spaces.",
     }
 
     # construct the argument parse and parse the arguments
     parser = ArgumentParser(usage=usage, description=description)
-
-    # Sync
-    # parser.add_argument('--aws_s3', help=helpers['aws_s3'], type=int, default=0)
-    # parser.add_argument('--aws_s3_bucket', help=helpers['aws_s3_bucket'], type=str)
     parser.add_argument('--domains', help=helpers['domain'], type=str)
 
     # Parse Arguments
