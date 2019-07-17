df1495e39ce317f2b6477e69341afd13f5cfae22
mrstephenneal <stephen.neal14@gmail.com>
2019-07-17 09:00:21 -0400

    MAKE 1.17-alpine-v5
    START creating /.well-known/acme-challenge/ redirects.

--- push.sh
+++ push.sh
@@ -9,3 +9,4 @@ docker push stephenneal/nginx-laravel:1.17-alpine-v1
 docker push stephenneal/nginx-laravel:1.17-alpine-v2
 docker push stephenneal/nginx-laravel:1.17-alpine-v3
 docker push stephenneal/nginx-laravel:1.17-alpine-v4
+docker push stephenneal/nginx-laravel:1.17-alpine-v5
