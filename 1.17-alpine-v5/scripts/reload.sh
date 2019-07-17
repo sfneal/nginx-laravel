3bacb315c1b6fff7a56f4510ec8e9768e2110ca1
mrstephenneal <stephen.neal14@gmail.com>
2019-07-17 15:33:02 -0400

    MAKE pull-certs.sh script for download certs during certify.sh and reload.sh

--- 1.17-alpine-v5/scripts/reload.sh
+++ 1.17-alpine-v5/scripts/reload.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
+# Download new certs created by validation server from AWS S3
+for d in ${domain}; do
+    sh /sites-scripts/pull-certs.sh ${d}
+done
+
 # Reload Nginx
 nginx -s reload
-
-# Upload certs to AWS S3
-# Check if aws_s3 is enabled via ENV vars (if staging != 0)
-if [[ ${aws_s3} -ne 0 ]] && [[ ${aws_s3_upload} -ne 0 ]];
-then
-    echo "Production environment enabled... uploading SSL certs to AWS S3."
-    awss3 upload --bucket ${aws_s3_bucket} \
-        --local_path /etc/letsencrypt/
-else
-    echo "Staging environment enabled... skipping AWS S3 SSL certs upload."
-fi
