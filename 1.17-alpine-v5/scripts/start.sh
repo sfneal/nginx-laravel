8301cfea3d37327e3bd35f52532a5eccf5dcf68e
mrstephenneal <stephen.neal14@gmail.com>
2019-07-17 10:15:36 -0400

    FIX validation_domain placeholder value

--- 1.17-alpine-v5/scripts/start.sh
+++ 1.17-alpine-v5/scripts/start.sh
@@ -18,7 +18,7 @@ fi
 # Replace $VALIDATION_DOMAIN placeholder with env variable value
 replace_domain --domain ${validation_domain} \
     --conf-file /etc/nginx/conf.d/default.conf \
-    --placeholder '$VALIDATION_DOMAIN'
+    --placeholder '@VALIDATION_DOMAIN'
 
 # Download/create SSL certs for each domain
 for d in ${domain}; do
