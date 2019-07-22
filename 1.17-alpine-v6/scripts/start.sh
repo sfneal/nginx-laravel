a2f281776c974dc482d72c8eb59c9d84c9944eb9
mrstephenneal <stephen.neal14@gmail.com>
2019-07-22 16:13:14 -0400

    CUT api_enabled env var from Dockerfile instructions

--- 1.17-alpine-v6/scripts/start.sh
+++ 1.17-alpine-v6/scripts/start.sh
@@ -6,15 +6,6 @@ then
     sh /scripts/aws-credentials.sh
 fi
 
-# Conditionally include app.conf with API exposure
-if [[ ${api_enabled} -gt 0 ]]
-then
-    echo "API serving is enabled..."
-else
-    echo "API serving is disabled..."
-    rm /etc/nginx/conf.d/api.conf
-fi
-
 # Replace @VALIDATION_DOMAIN placeholder with env variable value
 replace_domain --domain ${validation_domain} \
     --conf-file /etc/nginx/conf.d/default.conf \
