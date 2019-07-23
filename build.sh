208af16f4671f2954f4a86acdae0ab1d9c18bdb2
mrstephenneal <stephen.neal14@gmail.com>
2019-07-23 10:46:08 -0400

    MAKE v7 image with improved env variable construction

--- build.sh
+++ build.sh
@@ -9,3 +9,4 @@ docker build -t stephenneal/nginx-laravel:1.17-alpine-v3 ./1.17-alpine-v3/
 docker build -t stephenneal/nginx-laravel:1.17-alpine-v4 ./1.17-alpine-v4/
 docker build -t stephenneal/nginx-laravel:1.17-alpine-v5 ./1.17-alpine-v5/
 docker build -t stephenneal/nginx-laravel:1.17-alpine-v6 ./1.17-alpine-v6/
+docker build -t stephenneal/nginx-laravel:1.17-alpine-v7 ./1.17-alpine-v7/
