c81f4ff0b8c05f02fda5179e76e845e075d5eeda
mrstephenneal <stephen.neal14@gmail.com>
2019-08-08 09:44:26 -0400

    MAKE v10 image that will support node js content serving

--- push.sh
+++ push.sh
@@ -14,3 +14,4 @@ docker push stephenneal/nginx-laravel:1.17-alpine-v6
 docker push stephenneal/nginx-laravel:1.17-alpine-v7
 docker push stephenneal/nginx-laravel:1.17-alpine-v8
 docker push stephenneal/nginx-laravel:1.17-alpine-v9
+docker push stephenneal/nginx-laravel:1.17-alpine-v10
