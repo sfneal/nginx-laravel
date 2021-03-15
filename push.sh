bde51efd7f0da89fb9646a85fcdd835376f452f5
Stephen Neal <stephen.neal14@gmail.com>
2021-03-15 12:51:42 -0400

    ADD 'deploy' section to Travis CI config for pushing Docker images to Docker Hub

--- push.sh
+++ push.sh
@@ -2,21 +2,35 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sh "${DIR}"/build.sh
+# Optional TAG argument (if set, only the specified image will be built)
+TAG=${1:-null}
 
-docker push stephenneal/nginx-laravel:1.15-alpine
-docker push stephenneal/nginx-laravel:1.16-alpine
-docker push stephenneal/nginx-laravel:1.17
-docker push stephenneal/nginx-laravel:1.17-alpine-v1
-docker push stephenneal/nginx-laravel:1.17-alpine-v2
-docker push stephenneal/nginx-laravel:1.17-alpine-v3
-docker push stephenneal/nginx-laravel:1.17-alpine-v4
-docker push stephenneal/nginx-laravel:1.17-alpine-v5
-docker push stephenneal/nginx-laravel:1.17-alpine-v6
-docker push stephenneal/nginx-laravel:1.17-alpine-v7
-docker push stephenneal/nginx-laravel:1.17-alpine-v8
-docker push stephenneal/nginx-laravel:1.17-alpine-v9
-docker push stephenneal/nginx-laravel:1.17-alpine-v10
-docker push stephenneal/nginx-laravel:1.17-alpine-v11
-docker push stephenneal/nginx-laravel:1.18-alpine-v1
-docker push stephenneal/nginx-laravel:1.19-alpine-v1
+# Check if the TAG variable is set
+if [ "$TAG" != null ]
+
+  # Only build & push one image
+  then
+    sh "${DIR}"/build.sh "${TAG}"
+    docker push stephenneal/nginx-laravel:"${TAG}"
+
+  # Build & push all images
+  else
+    sh "${DIR}"/build.sh
+
+    docker push stephenneal/nginx-laravel:1.15-alpine
+    docker push stephenneal/nginx-laravel:1.16-alpine
+    docker push stephenneal/nginx-laravel:1.17
+    docker push stephenneal/nginx-laravel:1.17-alpine-v1
+    docker push stephenneal/nginx-laravel:1.17-alpine-v2
+    docker push stephenneal/nginx-laravel:1.17-alpine-v3
+    docker push stephenneal/nginx-laravel:1.17-alpine-v4
+    docker push stephenneal/nginx-laravel:1.17-alpine-v5
+    docker push stephenneal/nginx-laravel:1.17-alpine-v6
+    docker push stephenneal/nginx-laravel:1.17-alpine-v7
+    docker push stephenneal/nginx-laravel:1.17-alpine-v8
+    docker push stephenneal/nginx-laravel:1.17-alpine-v9
+    docker push stephenneal/nginx-laravel:1.17-alpine-v10
+    docker push stephenneal/nginx-laravel:1.17-alpine-v11
+    docker push stephenneal/nginx-laravel:1.18-alpine-v1
+    docker push stephenneal/nginx-laravel:1.19-alpine-v1
+fi
