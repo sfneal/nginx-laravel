87176e7672f8b4218db961bcd2ae47d16b548084
Stephen Neal <stephen.neal14@gmail.com>
2024-09-06 14:36:38 -0400

    OpenSSL RSA key length (increased to 2048) (#151)
    - fix openssl rsa key length to 2048 (from 1024)

--- 1.17/command.sh
+++ 1.17/command.sh
@@ -6,7 +6,7 @@ replace_domain --domain ${domain} --conf-file /etc/nginx/conf.d/default.conf
 # Create dummy certificate for ${domain}
 echo "Creating dummy certificate for ${domain}"
 mkdir -m 777 -p /etc/letsencrypt/live/${domain}/ \
-    && openssl req -x509 -nodes -newkey rsa:1024 -days 1 \
+    && openssl req -x509 -nodes -newkey rsa:2048 -days 1 \
         -keyout "/etc/letsencrypt/live/${domain}/privkey.pem" \
         -out "/etc/letsencrypt/live/${domain}/fullchain.pem" \
         -subj "/CN=localhost"
