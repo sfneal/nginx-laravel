c0c9782f2f9dab797d0d64f757be47d8aa153686
sfneal <stephen.neal14@gmail.com>
2025-01-06 16:05:56 -0500

    ADD multi-platform build configs so that images can be built for arm64 & amd64 platforms

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Optional TAG argument (if set, only the specified image will be built)
 TAG=${1:-null}
 
+# Optional PLATFORM argument (if none provided, both will be built)
+PLATFORM=${2:-"linux/amd64,linux/arm64"}
+
 # Check if the TAG variable is set
 if [ "$TAG" != null ]
 
@@ -22,11 +25,15 @@ if [ "$TAG" != null ]
         TAGS="${TAGS} -t stephenneal/nginx-laravel:${line}"
       done < "${DIR}"/"${TAG}"/_docker-tags.txt
 
-      COMMAND="docker build ${TAGS} ${DIR}/${TAG}/"
+      COMMAND="docker buildx build --load ${TAGS} --platform "${PLATFORM}" ${DIR}/${TAG}/"
       echo "${COMMAND}"
       $(echo "${COMMAND}")
     else
-      docker build -t stephenneal/nginx-laravel:"${TAG}" "${DIR}"/"${TAG}"/
+      docker buildx build \
+      	--load \
+      	-t stephenneal/nginx-laravel:"${TAG}" \
+      	--platform "${PLATFORM}" \
+      	"${DIR}"/"${TAG}"/
     fi
 
   # Build all images
